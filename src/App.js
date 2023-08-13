ba9d448eca64a6ce5c8a692c045d4784c89704b1
Thambara Sahassaka <thambara.20@cse.mrt.ac.lk>
2023-08-13 17:08:43 +0530

    updated 5

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import './App.css';
+import Toggle from './PortfolioContainer/Toggle/Toggle';
 import Profile from './PortfolioContainer/Home/Profile';
 import AboutMain from './PortfolioContainer/AboutMe/AboutMain';
+import { useContext} from 'react';
+import { ThemeContext } from './PortfolioContainer/Context';
 
 function App() {
-
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
  
 
   return (
     <div className="App" style={{
-      backgroundColor:  "black",
-      color: "white",
+      backgroundColor: darkMode ? "black" : "white",
+      color: darkMode && "white",
     }}>
-      
+      <Toggle />
       <Profile />
       <AboutMain />
    
--- src/index.js
+++ src/index.js
@@ -7,10 +7,11 @@ import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import 'bootstrap/dist/js/bootstrap.bundle';
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from './PortfolioContainer/Context';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+   <ThemeProvider><App /></ThemeProvider>
   </React.StrictMode>
 );
 
